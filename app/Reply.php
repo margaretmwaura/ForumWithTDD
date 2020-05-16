0c6c9abd6dce2a57180639af01c553b4554ce8c4
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-05-16 10:25:12 +0300

    adding the replies viewing , relationships and testing

--- app/Reply.php
+++ app/Reply.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+
+    // We had to add the foreign key because the name of the relationship is not similar to that of the class
+    public function owner()
+    {
+        return $this->belongsTo(User::class , 'user_id');
+    }
 }
